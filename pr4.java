12cbb39dbf81e7e8dd0feb5b853590f8656a6168
Krupa  J. Baldha <krupabld12@gmail.com>
2026-04-28 18:29:45 +0530

    Add MergeSort class with sorting functionality
    Implement Merge Sort algorithm with user input.

---
+++ pr4.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class MergeSort{
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+        // Print after each merge (acts like pass)
+        System.out.print("Merge [" + left + "," + right + "]: ");
+        printArray(arr);
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.print("Original array: ");
+        printArray(arr);
+ 
+/* output 
+Enter number of elements: 5
+Enter elements:
+10
+8
+4
+5
+20
+Original array: 10 8 4 5 20 
+Merge [0,1]: 8 10 4 5 20 
+Merge [0,2]: 4 8 10 5 20 
+Merge [3,4]: 4 8 10 5 20 
+Merge [0,4]: 4 5 8 10 20 
+Execution Time: 4511990 nanoseconds
+Sorted array: 4 5 8 10 20 */
